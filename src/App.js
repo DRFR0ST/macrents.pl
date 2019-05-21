031e014ba0820624710d55b358480fd0140956d6
Mike Eling <mike.eling97@gmail.com>
2019-05-21 02:24:06 +0200

    Change app name

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.png';
-import './App.css';
+import React from "react";
+import logo from "./logo.png";
+import "./App.css";
 
 import Tilt from "react-tilt";
 
@@ -9,21 +9,20 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Strona w budowie...
-        </p>
+        <p>Strona w budowie...</p>
         <a
           className="App-link"
           href="https://facebook.com/"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Tilt className="Tilt" options={{ max : 40, glare: true, maxGlare: 0.5 }} >
- <div className="Tilt-inner"> O NAS </div>
-</Tilt>
+          rel="noopener noreferrer">
+          <Tilt
+            className="Tilt"
+            options={{ max: 40, glare: true, maxGlare: 0.5 }}>
+            <div className="Tilt-inner"> O NAS </div>
+          </Tilt>
         </a>
       </header>
-      <div class="image-footer"></div>
+      <div class="image-footer" />
     </div>
   );
 }
