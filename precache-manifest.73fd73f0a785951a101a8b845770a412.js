86bcee98f200cd8f5358482a428608186f9696b4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-24 17:18:43 +0000

    Deploy DRFR0ST/vmrents-website to github.com/DRFR0ST/vmrents-website.git:gh-pages

---
+++ precache-manifest.73fd73f0a785951a101a8b845770a412.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b571f80856fa9ffca6ab59580df659ea",
+    "url": "/vmrents-website/index.html"
+  },
+  {
+    "revision": "3284209e06a1b5b2a4cc",
+    "url": "/vmrents-website/static/css/main.6fb6c847.chunk.css"
+  },
+  {
+    "revision": "00b5edaee422c7a2eb76",
+    "url": "/vmrents-website/static/js/2.b45daff1.chunk.js"
+  },
+  {
+    "revision": "3284209e06a1b5b2a4cc",
+    "url": "/vmrents-website/static/js/main.c3ecace2.chunk.js"
+  },
+  {
+    "revision": "c3f1d09cdeb9fd13002a",
+    "url": "/vmrents-website/static/js/runtime~main.19a53148.js"
+  },
+  {
+    "revision": "a2c6954f0ea8ef9a63548c9f0aff450f",
+    "url": "/vmrents-website/static/media/exampleVehicle.a2c6954f.jpg"
+  },
+  {
+    "revision": "5aa017b7323f04bd5d13bcb7139e4a99",
+    "url": "/vmrents-website/static/media/exampleVehicle3.5aa017b7.png"
+  },
+  {
+    "revision": "471f8e08020f282cbe26f2bc975b8895",
+    "url": "/vmrents-website/static/media/exampleVehicle4.471f8e08.png"
+  },
+  {
+    "revision": "69ef95bab2c1fa5a086167f2a5d0653f",
+    "url": "/vmrents-website/static/media/exampleVehicle5.69ef95ba.png"
+  },
+  {
+    "revision": "030b7a050cc1f5fbb4a1d0a8ba8f870c",
+    "url": "/vmrents-website/static/media/exampleVehicle6.030b7a05.jpg"
+  },
+  {
+    "revision": "58d8ddfe24cdb47737bec5d490cd1baf",
+    "url": "/vmrents-website/static/media/exampleVehicle7.58d8ddfe.jpg"
+  },
+  {
+    "revision": "62bd27a1a0612bca6aa8bc5d64412c30",
+    "url": "/vmrents-website/static/media/header_car2.62bd27a1.png"
+  },
+  {
+    "revision": "c1bc2118c048e0561a9edcad176ea815",
+    "url": "/vmrents-website/static/media/logo.c1bc2118.png"
+  }
+]);
--- src/components/pages/Fleet.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import FleetSelector from '../common/FleetSelector'
-
-const Fleet = () => {
-  return <FleetSelector />
-}
-
-export default Fleet
