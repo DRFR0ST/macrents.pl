34818e376f412624f46bb2c7e0f7d907a650abe8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-20 17:55:37 +0000

    Deploy DRFR0ST/vmrents-website to github.com/DRFR0ST/vmrents-website.git:gh-pages

---
+++ precache-manifest.9690dff1c62d3b9d1c75f0695a985919.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9818b501523e5d591c9668d4acb6c8ac",
+    "url": "/macrents.pl/index.html"
+  },
+  {
+    "revision": "aec953e6cfea11f73ef6",
+    "url": "/macrents.pl/static/css/main.16867338.chunk.css"
+  },
+  {
+    "revision": "ccc2a835778cee2a2e1a",
+    "url": "/macrents.pl/static/js/2.8e649eb0.chunk.js"
+  },
+  {
+    "revision": "aec953e6cfea11f73ef6",
+    "url": "/macrents.pl/static/js/main.ae2c2ae1.chunk.js"
+  },
+  {
+    "revision": "7eacdae345c51d9f60dd",
+    "url": "/macrents.pl/static/js/runtime~main.c65b2369.js"
+  },
+  {
+    "revision": "80511a954059892476941c6b77fb1789",
+    "url": "/macrents.pl/static/media/audi_q7.80511a95.png"
+  },
+  {
+    "revision": "99486e4ca4032d2ce1e8148d97721a5b",
+    "url": "/macrents.pl/static/media/bmg_4_black.99486e4c.png"
+  },
+  {
+    "revision": "99486e4ca4032d2ce1e8148d97721a5b",
+    "url": "/macrents.pl/static/media/bmw_4_white.99486e4c.png"
+  },
+  {
+    "revision": "eb68909988cde4b26248f4e15d14f791",
+    "url": "/macrents.pl/static/media/bmw_5.eb689099.png"
+  },
+  {
+    "revision": "02e5e5e55500a57fae116ccb60ac77e3",
+    "url": "/macrents.pl/static/media/header_car3.02e5e5e5.png"
+  },
+  {
+    "revision": "87b7ad8f0e037d269bc81b55a34f985d",
+    "url": "/macrents.pl/static/media/jaguar.87b7ad8f.png"
+  },
+  {
+    "revision": "b439106b06a9937320225d716bd1e5bd",
+    "url": "/macrents.pl/static/media/logo.b439106b.png"
+  },
+  {
+    "revision": "21c1572f83c800d778afb9f657f8b102",
+    "url": "/macrents.pl/static/media/mercedes_a_grey.21c1572f.png"
+  },
+  {
+    "revision": "e9ca9263ca08de7a72cf06c35333dfd0",
+    "url": "/macrents.pl/static/media/mercedes_e_amg_grey.e9ca9263.png"
+  },
+  {
+    "revision": "a1c6e487cf464788f83daf5d6562ebea",
+    "url": "/macrents.pl/static/media/mercedes_e_black.a1c6e487.png"
+  },
+  {
+    "revision": "d165b1f3e8c603e8a70ce4809d79c45f",
+    "url": "/macrents.pl/static/media/skoda_octavia_combi.d165b1f3.png"
+  },
+  {
+    "revision": "e7dcfd284b6c190e0f8ada5dff216672",
+    "url": "/macrents.pl/static/media/skoda_oktavia_sedan.e7dcfd28.png"
+  },
+  {
+    "revision": "295cea048fb6bfd40d83f4fec9b01667",
+    "url": "/macrents.pl/static/media/skoda_rapid.295cea04.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vmrents-website/precache-manifest.d7d794729d25bc59fe71e9f3672c9cc3.js"
+  "/macrents.pl/precache-manifest.9690dff1c62d3b9d1c75f0695a985919.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vmrents-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/macrents.pl/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
