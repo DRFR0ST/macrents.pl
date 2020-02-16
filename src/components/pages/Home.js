fb2d2d43e3c26856c5289750c661222cabfa7685
Mike Eling <mike.eling97@gmail.com>
2020-02-16 22:32:47 +0100

    :lipstick: Add sales section

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,8 +4,9 @@ import Header from '../common/Header';
 import PropTypes from 'prop-types';
 import React from 'react';
 import Special from '../common/Special';
-import {withStyles } from '@material-ui/core';
-//import FleetSelector from '../common/FleetSelector';
+import { Icon, Typography, withStyles } from '@material-ui/core';
+import FleetSelector from '../common/FleetSelector';
+import vehImage from '../../images/newfleet/audi_q7.png';
 
 const styles = {
   root: {},
@@ -16,35 +17,90 @@ const Home = ({ children, classes }) => {
   return (
     <div className={classes.root}>
       <Header />
-      {/* 
+
       <div
         style={{
           display: 'flex',
           alignItems: 'flex-start',
-          maxHeight: '100vh',
-          maxWidth: '90%',
-          overflow: 'hidden',
+          minHeight: '100vh',
+          maxWidth: '100%',
+          flexWrap: 'wrap',
         }}
       >
-        <div style={{ maxWidth: '45%' }}>
+        <div
+          style={{
+            maxWidth: '50%',
+            height: '100vh',
+            position: 'relative',
+            minWidth: '400px',
+          }}
+        >
+          <div
+            style={{
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              zIndex: 4,
+              display: 'flex',
+              alignItems: 'center',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              height: '100%',
+            }}
+          >
+            <Typography variant="h3" color="primary">
+              BRAK PROMOCJI
+            </Typography>
+            <img
+              src={vehImage}
+              style={{
+                filter: 'brightness(0%) opacity(0.7)',
+                marginTop: '2rem',
+                maxWidth: '100%',
+              }}
+            />
+          </div>
+          <div
+            style={{
+              zIndex: 0,
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              width: '100%',
+              height: '100%',
+              background:
+                'linear-gradient(to top, rgba(255, 255, 255, 0.4), transparent 60%)',
+            }}
+          ></div>
+        </div>
+        <div
+          style={{
+            height: '100vh',
+            width: '49%',
+            textAlign: 'center',
+            display: 'flex',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            alignItems: 'center',
+            minWidth: '400px',
+          }}
+        >
           <Typography
             style={{ fontWeight: 'bold' }}
             variant="h3"
             align="center"
             color="primary"
           >
-            Okazy floty
+            Aktualne promocje
           </Typography>
-          <FleetSelector maxItems={3} />
+          <Icon style={{ fontSize: '35vw' }} color="primary">
+            money_off
+          </Icon>
         </div>
-        <div style={{ maxWidth: '45%' }}>
-          <Icon
-            name="percentage"
-            style={{ fontSize: '45vw' }}
-            color="primary"
-          />
-        </div>
-      </div> */}
+      </div>
       <div className={classes.content}>
         <Special />
       </div>
