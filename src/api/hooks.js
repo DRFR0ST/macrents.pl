0ea6e3a84e1e3c1c422aa0e68178c2ebb726204b
Mike Eling <mike.eling97@gmail.com>
2020-09-26 15:40:19 +0200

    :wrench: Fix warnings

--- src/api/hooks.js
+++ src/api/hooks.js
@@ -4,7 +4,7 @@ import { useState, useCallback, useRef } from 'react';
 // const templateType = (type) => {};
 
 const validateEmail = (email) => {
-  const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   return {
     valid: Boolean(regex.test(String(email).toLowerCase())),
